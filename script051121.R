205ea17743e58a41b4a3842eedcf5a7bb5a2aec6
Antonela Tamagnini <54758161+atamagnini@users.noreply.github.com>
2021-11-09 14:25:13 -0300

    Add files via upload

---
+++ script051121.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(highcharter)
+library(htmlwidgets)
+
+dataset <- read.csv('D:/R scripts/script_051121_internet_users/data.csv', sep = ';', header = T)
+df<- as.data.frame(dataset)
+#clean dataset
+colnames(df)[1] <- 'Country'
+df$Country <- str_trim(df$Country, side = 'both')
+glimpse(df)
+df$X <- round(df$Internet.users/df$Population, 2)*100
+df[df == 'Russian Federation'] <- 'Russia'
+df[df == 'Bolivia, Plurnational State of'] <- 'Bolivia'
+df[df == 'Venezuela, Bolivarian Republic of'] <- 'Venezuela'
+df[df == 'Iran, Islamic Republic of'] <- 'Iran'
+df[df == 'United Kingdom of Great Britain and Northern Ireland'] <- 'United Kingdom'
+#valores ausentes y condicionales
+any(is.na(df$X))
+df2 <- df[complete.cases(df), ]
+df3 <- df2[df2$X <= 100, ]
+glimpse(df3)
+#map
+#map_data <- get_data_from_map(download_map_data("custom/world"))
+p <- hcmap(map = 'custom/world', download_map_data = getOption("highcharter.download_map_data"), data = df3,
+           value = 'X', joinBy = c('name', 'Country')) %>%
+  hc_tooltip(useHTML = TRUE, headerFormat = '<b>Percentage of internet users<br>',
+             pointFormat = '{point.name}:{point.value}%') %>%
+  hc_title(text = 'World Internet users', 
+           style = list(fontWeight = 'bold', fontSize = '20px'),
+           align = 'left') %>%
+  hc_credits(enabled = TRUE, text = 'Map by Antonela Tamagnini
+             <br> Source: Wikipedia') %>%
+  hc_mapNavigation(enabled = TRUE)
+p
+saveWidget(widget = p, file = "plot.html")
